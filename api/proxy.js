8c65697aef48e9f3d1a5702c452bf8928f87c1be
KiritoKing <kiritoclzh@gmail.com>
2022-01-07 06:44:31 +0800

    rewrite proxy path

--- api/proxy.js
+++ api/proxy.js
@@ -15,5 +15,10 @@ module.exports = (req, res) => {
   createProxyMiddleware({
     target,
     changeOrigin: true,
+    pathRewrite: {
+      // 通过路径重写，去除请求路径中的 `/backend`
+      // 例如 /backend/user/login 将被转发到 http://backend-api.com/user/login
+      '^/graphql/': '/'
+    },
   })(req, res)
 }
