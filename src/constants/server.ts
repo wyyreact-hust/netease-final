ec2fca34502520442877dd4ffd3ea603c59b4647
KiritoKing <kiritoclzh@gmail.com>
2022-01-07 06:50:45 +0800

    local mode

--- src/constants/server.ts
+++ src/constants/server.ts
@@ -1,4 +1,4 @@
-export const SERVER = 'https://netease-cloud-music-api-psi-murex.vercel.app'
-export const GRAPHQL_SERVER = '/graphql'
-
 const GraphRemote = 'http://www.uniquemo.cn'
+
+export const SERVER = 'https://netease-cloud-music-api-psi-murex.vercel.app'
+export const GRAPHQL_SERVER = GraphRemote + '/graphql'
