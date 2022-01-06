d4fa859b1cb32bbf932579f3dd174db15f24b6cf
KiritoKing <kiritoclzh@gmail.com>
2022-01-07 01:56:41 +0800

    add http-middleware

--- src/constants/server.ts
+++ src/constants/server.ts
@@ -1,2 +1,2 @@
 export const SERVER = 'https://netease-cloud-music-api-psi-murex.vercel.app'
-export const GRAPHQL_SERVER = 'http://www.uniquemo.cn/graphql'
+export const GRAPHQL_SERVER = '/graphql'
